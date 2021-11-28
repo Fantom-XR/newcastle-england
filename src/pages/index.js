23bffc32d8176d6ebe8a14bdee8688a795596db3
Fantom XR <52836243+Fantom-XR@users.noreply.github.com>
2021-11-28 11:08:05 +0000

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,35 +1,68 @@
-import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
-
-import Layout from "../components/layout"
-import Seo from "../components/seo"
-
-const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-  <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
-      <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
-      <Link to="/using-dsg">Go to "Using DSG"</Link>  
-  <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["auto", "webp", "avif"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
-      <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
-      <Link to="/using-dsg">Go to "Using DSG"</Link>
-    </p>
-  </Layout>
-)
-
-export default IndexPage
+document.write('');
+document.write('<!DOCTYPE html>');
+document.write('<html>');
+document.write('    <head>');
+document.write('    <title>Home | Unable2017</title>');
+document.write('<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">');
+document.write('<style>');
+document.write('  .comment {');
+document.write('  padding: 10px;');
+document.write('  border-color: #ffffff; ');
+document.write('  color: white;');
+document.write('  margin-bottom: 15px;');
+document.write('border-style: solid;  ');
+document.write('    border-width: 2px;');
+document.write('}');
+document.write('</style>');
+document.write('');
+document.write('');
+document.write('');
+document.write('<nav class="navbar navbar-expand-lg">');
+document.write('  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">');
+document.write('    <span class="navbar-toggler-icon"></span>');
+document.write('  </button>');
+document.write('  <div class="collapse navbar-collapse" id="navbarNav">');
+document.write('    <ul class="navbar-nav">');
+document.write('      <li class="nav-item active">');
+document.write('        <a class="nav-link text-white" href="/index.php">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Home</a>');
+document.write('      </li>');
+document.write('');
+document.write('<li class="nav-item active">');
+document.write('    <a class="nav-link text-white" href="/contact/contact.php">Contact Us</a> ');
+document.write('      </li>');
+document.write('');
+document.write('<li class="nav-item active">');
+document.write('        <a class="nav-link text-white" href="/modapps/apply.php">Staff Application</a>');
+document.write('      </li>      ');
+document.write('');
+document.write('<li class="nav-item active">');
+document.write('        <a class="nav-link text-white" href="/staff.php">Staff Team</a>');
+document.write('      </li> ');
+document.write('');
+document.write('<li class="nav-item active">');
+document.write('        <a class="nav-link text-white" href="/privacy.php">Privacy Policy</a>');
+document.write('      </li>');
+document.write('<br>');
+document.write(' </ul>');
+document.write('');
+document.write('  </div>    ');
+document.write('');
+document.write('</nav>');
+document.write('<body>');
+document.write(' <body style="background-color:#2C2C2C;">   ');
+document.write('');
+document.write(' ');
+document.write('<main class="container">');
+document.write('  <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center text-light">');
+document.write('    <h1 class="display-4">Unable2017\'s Studio</h1>');
+document.write('    <p class="lead">Welcome to Unable2017\'s website, dedicated to game development on ROBLOX.</p>');
+document.write('    <a href="/contact/contact.php"><button type="button" class="btn btn-outline-light">Create a Ticket</button></a>');
+document.write('    <a href="/login/index.php"><button type="button" class="btn btn-outline-light">Sign In</button> </a>');
+document.write('      ');
+document.write('    </div>');
+document.write('');
+document.write('  </div>');
+document.write('');
+document.write('<div class="container">        ');
+document.write('   <p class="comment text-center"><b>We are currently looking for game developers. Please email contact@unable2017.com should you wish to apply for this position.</b></p>');
+document.write('');
